94109bdce2cab17e00df7b576a3a6524fde734a9
Katerina Solstad (katya sorok) <katya.solstad@gmail.com>
2020-10-06 16:37:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+'''
+Boolean operations
+x or y   
+x and y
+not x
+not a==b is the same as not (a==b) (error!!!: a== not b)
+<=
+>
+>=
+==
+!=
+is (for objects)
+is not (negated for objects)
+
+LOGIC:
+
+T and T -> T
+T and F -> F
+F and F -> F
+
+T or T ->  T
+T or F ->  T
+F or F ->  F
+'''
